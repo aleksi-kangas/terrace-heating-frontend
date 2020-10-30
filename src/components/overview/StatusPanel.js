548c8591fa16d81296909a4d09fcc12ec14d1794
Aleksi Kangas <aleksi.kangas@hotmail.com>
2020-10-30 19:01:36 +0200

    Refactor: Minor unnecessary styling removed

--- src/components/overview/StatusPanel.js
+++ src/components/overview/StatusPanel.js
@@ -40,32 +40,32 @@ const StatusPanel = ({ data }) => {
       <Grid item container justify='center'>
         <Grid item component={Card} className={clsx(classes.card, classes.active)}>
           <CardContent>
-            <Typography variant='h5' gutterBottom align='center'>Circuit 1</Typography>
-            <Typography color='textSecondary' gutterBottom align='center' component={'span'}>
+            <Typography variant='h5' align='center'>Circuit 1</Typography>
+            <Typography color='textSecondary' align='center' component={'span'}>
               { activeCircuits ? 'Active' : <CircularProgress/> }
             </Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card} className={clsx(classes.card, classes.active)}>
           <CardContent>
-            <Typography variant='h5' gutterBottom align='center'>Circuit 2</Typography>
-            <Typography color='textSecondary' gutterBottom align='center' component={'span'}>
+            <Typography variant='h5' align='center'>Circuit 2</Typography>
+            <Typography color='textSecondary' align='center' component={'span'}>
               { activeCircuits ? 'Active' : <CircularProgress/> }
             </Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card} className={clsx(classes.card, activeCircuits < 3 ? classes.stopped : classes.active)}>
           <CardContent>
-            <Typography variant='h5' gutterBottom align='center'>Circuit 3</Typography>
-            <Typography color='textSecondary' gutterBottom align='center' component={'span'}>
+            <Typography variant='h5' align='center'>Circuit 3</Typography>
+            <Typography color='textSecondary' align='center' component={'span'}>
               { activeCircuits === 3 ? 'Active' : 'Stopped' }
             </Typography>
           </CardContent>
         </Grid>
         <Grid item component={Card} className={clsx(classes.card, classes.active)}>
           <CardContent>
-            <Typography variant='h5' gutterBottom align='center'>Latest update</Typography>
-            <Typography color='textSecondary' gutterBottom align='center' component={'span'}>
+            <Typography variant='h5' align='center'>Latest update</Typography>
+            <Typography color='textSecondary' align='center' component={'span'}>
               {latestUpdate ? latestUpdate : <CircularProgress/>}
             </Typography>
           </CardContent>
